76f5fcf8ff381d3d7a155486ef1d412527e3ccfa
Hendri Pretorius <pretorh@gmail.com>
2013-11-06 22:45:28 +0200

    argument passing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <libgen.h>
 
-int main() {
+struct Backup {
+    bool isFull;
+    char *dir;
+    char *file;
+};
+
+void printUsage() {
+    printf("Invalid arguments:\n");
+    printf("\taction [f|full|i|incremental]\n");
+    printf("\tbackup descriptor file\n");
+    exit(1);
+}
+
+void getAction(const char *argv, struct Backup *backup) {
+    bool isFull = strcmp(argv, "f") == 0 || strcmp(argv, "full") == 0;
+    bool isIncremental = strcmp(argv, "i") == 0 || strcmp(argv, "incremental") == 0;
+    if (!isFull && !isIncremental) {
+        printUsage();
+    }
+    backup->isFull = isFull;
+}
+
+void getDescriptorFile(const char *argv, struct Backup *backup) {
+    backup->dir = dirname(strdup(argv));
+    backup->file = basename(strdup(argv));
+}
+
+int main(int argc, const char **argv) {
+    struct Backup backup;
+
+    if (argc != 3) {
+        printUsage();
+    }
+
+    getAction(argv[1], &backup);
+    getDescriptorFile(argv[2], &backup);
+
+    printf("Action: %s\n", backup.isFull ? "full" : "incremental");
+    printf("Dir   : %s\n", backup.dir);
+    printf("File  : %s\n", backup.file);
+
     printf("bye\n");
     return 0;
 }
