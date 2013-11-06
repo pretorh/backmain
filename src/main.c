0a8c7d11d17233af5e6fb1515295e7b5cab2caf8
Hendri Pretorius <pretorh@gmail.com>
2013-11-06 23:21:28 +0200

    setup data directory

--- src/main.c
+++ src/main.c
@@ -3,11 +3,17 @@
 #include <string.h>
 #include <stdbool.h>
 #include <libgen.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 struct Backup {
     bool isFull;
     char *dir;
     char *file;
+    char *rootDir;
+    char *mirrorDir;
+    char *newDir;
 };
 
 void printUsage() {
@@ -29,6 +35,33 @@ void getAction(const char *argv, struct Backup *backup) {
 void getDescriptorFile(const char *argv, struct Backup *backup) {
     backup->dir = dirname(strdup(argv));
     backup->file = basename(strdup(argv));
+    
+    backup->rootDir = strdup(backup->file);
+    strcat(backup->rootDir, ".dat/");
+    
+    backup->mirrorDir = strdup(backup->rootDir);
+    strcat(backup->mirrorDir, "mirror/");
+
+    backup->newDir = strdup(backup->rootDir);
+    strcat(backup->newDir, "new/");
+}
+
+void makeDirOrFail(const char *path) {
+    if (access(path, F_OK) && (mkdir(path, S_IRWXU))) {
+        printf("Failed to create directory %s: %s\n", path, strerror(errno));
+        exit(1);
+    }
+}
+
+void setupDataDir(struct Backup *backup) {
+    if (chdir(backup->dir)) {
+        printf("Failed to change to data directory: %s\n", strerror(errno));
+        exit(1);
+    }
+   
+    makeDirOrFail(backup->rootDir);
+    makeDirOrFail(backup->mirrorDir);
+    makeDirOrFail(backup->newDir);
 }
 
 int main(int argc, const char **argv) {
@@ -44,6 +77,11 @@ int main(int argc, const char **argv) {
     printf("Action: %s\n", backup.isFull ? "full" : "incremental");
     printf("Dir   : %s\n", backup.dir);
     printf("File  : %s\n", backup.file);
+    printf("Root  : %s\n", backup.rootDir);
+    printf("Mirror: %s\n", backup.mirrorDir);
+    printf("New   : %s\n", backup.newDir);
+
+    setupDataDir(&backup);
 
     printf("bye\n");
     return 0;
